ceba47982d2c6a7dae6fc3d69c19b8888c70d125
skuld2000 <skuld2000@hanmail.net>
2023-09-08 11:01:15 +0900

    #6.11

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -1,10 +1,57 @@
 import 'package:flutter/material.dart';
 
 class DetailScreen extends StatelessWidget {
-  const DetailScreen({super.key});
+  final String title, thumb, id;
+
+  const DetailScreen({
+    super.key,
+    required this.title,
+    required this.thumb,
+    required this.id,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 2,
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.green,
+        title: Text(
+          title,
+          style: TextStyle(
+            fontSize: 24,
+          ),
+        ),
+      ),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 50,
+          ),
+          Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+            Hero(
+              tag: id,
+              child: Container(
+                width: 250,
+                clipBehavior: Clip.hardEdge,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  boxShadow: [
+                    BoxShadow(
+                      blurRadius: 10,
+                      offset: const Offset(10, 10),
+                      color: Colors.black.withOpacity(0.3),
+                    )
+                  ],
+                ),
+                child: Image.network(thumb), //webtoon.thumb),
+              ),
+            ),
+          ]),
+        ],
+      ),
+    );
   }
 }
